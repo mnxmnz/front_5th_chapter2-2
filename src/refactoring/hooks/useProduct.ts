61643a240e637c525b0f0fa5b7878e21d502dd4a
mnxmnz <minzidev@gmail.com>
2025-04-22 00:34:03 +0900

    feat: useProducts 훅에 제품 업데이트 기능 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,15 @@ import { useState } from 'react';
 import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts(prevProducts => prevProducts.map(p => (p.id === updatedProduct.id ? updatedProduct : p)));
+  };
+
   return {
-    products: [...initialProducts],
-    updateProduct: () => undefined,
+    products,
+    updateProduct,
     addProduct: () => undefined,
   };
 };
