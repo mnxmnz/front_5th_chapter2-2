b785836f637e5fd96b79332f7fea0f88742de96b
mnxmnz <minzidev@gmail.com>
2025-04-21 23:56:24 +0900

    fix: useProducts 훅에서 초기 제품 배열을 반환하도록 수정

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import { Product } from "../../types.ts";
+import { useState } from 'react';
+import { Product } from '../../types.ts';
 
 export const useProducts = (initialProducts: Product[]) => {
   return {
-    products: [],
+    products: [...initialProducts],
     updateProduct: () => undefined,
     addProduct: () => undefined,
   };
